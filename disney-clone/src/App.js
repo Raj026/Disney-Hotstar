4f4a6c9faa885f1048811d9183102a97e23c6edf
Raj026 <rajpatani31@gmail.com>
2024-01-10 20:02:52 +0530

    Add ImageSlider and Videocards

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Login/>}>
           </Route>
-          <Route path="/" element={<Home/>}>
+          <Route path="/home" element={<Home/>}>
           </Route>
         </Routes>
       </Router>
